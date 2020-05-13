bc35718e8e84d6eb9125e4c1f5e4077d13dffae6
Andrey <zmet666zmey@mail.ru>
2020-05-13 19:18:26 +0300

    oneGitCod

---
+++ testGit/testGit/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  testGit
+//
+//  Created by Андрей on 13.05.2020.
+//  Copyright © 2020 Andreyos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
